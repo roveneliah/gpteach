2dd97e9fbbce93fa5efe15c7a3fa9fb0cb5699e6
Eli <ehanover21@gmail.com>
2023-02-13 17:29:49 -0500

    refactor out of route

--- lib/openai/index.ts
+++ lib/openai/index.ts
@@ -11,3 +11,62 @@ export const complete = async (prompt: string, length?: number) =>
     max_tokens: length || 50,
     temperature: 0.7,
   });
+
+export const createCurriculum = async (topic: string) => {
+  const prompt = `Create an instance of the following JSON schema for a learning curriculum.  
+  
+  This curriculum takes a test-driven approach, starting by identifying tests that the students must pass to demonstrate proficiency in some area.
+  The schema allows for fractal nesting of tests, so that a test can be composed of other tests.  This allows for a curriculum to be composed of a series of tests, each of which is composed of a series of tests, and so on.  Please take advantage of this feature.
+  Passing criteria should be very specific, articulating specific facts or details the student should demonstrate in order to pass.
+
+  {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Test Driven Curriculum",
+    "type": "object",
+    "properties": {
+        "tests": {
+            "type": "array",
+            "items": {
+                "$ref": "#/definitions/Test"
+            }
+        }
+    },
+    "definitions": {
+        "Test": {
+            "type": "object",
+            "properties": {
+                "test_id": {
+                    "type": "string"
+                },
+                "test_name": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "passing_criteria": {
+                    "type": "string"
+                },
+                "tests": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/Test"
+                    }
+                }
+            },
+            "required": [
+                "test_id",
+                "test_name",
+                "description",
+                "passing_criteria"
+            ]
+        }
+    }
+}
+
+
+Generate an instance of this schema for learning ${topic}.`;
+
+  const response = await complete(prompt, 2000);
+  return response.data.choices[0].text;
+};
--- src/pages/api/curriculum.ts
+++ src/pages/api/curriculum.ts
@@ -1,72 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { complete } from "lib/openai";
+import { createCurriculum } from "lib/openai";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Object>
 ) {
-  // the req body has the prompt we will send to OpenAI api
-  const prompt = `Create an instance of the following JSON schema for a learning curriculum.  
-  
-  This curriculum takes a test-driven approach, starting by identifying tests that the students must pass to demonstrate proficiency in some area.
-  The schema allows for fractal nesting of tests, so that a test can be composed of other tests.  This allows for a curriculum to be composed of a series of tests, each of which is composed of a series of tests, and so on.  Please take advantage of this feature.
-
-  {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "title": "Test Driven Curriculum",
-    "type": "object",
-    "properties": {
-        "tests": {
-            "type": "array",
-            "items": {
-                "$ref": "#/definitions/Test"
-            }
-        }
-    },
-    "definitions": {
-        "Test": {
-            "type": "object",
-            "properties": {
-                "test_id": {
-                    "type": "string"
-                },
-                "test_name": {
-                    "type": "string"
-                },
-                "description": {
-                    "type": "string"
-                },
-                "passing_criteria": {
-                    "type": "string"
-                },
-                "tests": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/Test"
-                    }
-                }
-            },
-            "required": [
-                "test_id",
-                "test_name",
-                "description",
-                "passing_criteria"
-            ]
-        }
-    }
-}
-
-
-Generate an instance of this schema for learning ${req.body.prompt}.`;
-
   // call the OpenAI api with the prompt
-  complete(prompt, 2000).then((response: any) => {
-    console.log(response.data.choices[0].text);
+  createCurriculum(req.body.prompt).then((curriculumRaw: any) => {
+    // if curriculum cannot be parser, return error
+    const curriculum = JSON.parse(curriculumRaw);
+
+    if (!curriculum) {
+      res.status(500).json({
+        topic: req.body.prompt,
+        completion: curriculumRaw,
+        error: "Could not parse curriculum",
+      });
+    }
+
+    console.log("curriculum", curriculum);
 
     res.status(200).json({
-      prompt,
-      completion: response.data.choices[0].text,
+      topic: req.body.prompt,
+      completion: curriculumRaw,
+      curriculum,
+      tests: curriculum.tests[0],
     });
   });
 }
